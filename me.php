7597d80d0676b5088be0df4026d34fd31daa6a6d
MATIAS IAIR BIDERMAN <41824396@ort>
2016-08-05 11:34:21 -0300

    Cerrar sesion y carpeta me con saludo de bienvenida

--- me.php
+++ me.php
@@ -1,5 +1,13 @@
 <?php
 require_once("conexion.php");
 session_start();
-json(Array("Mensaje" => "Bienvenido ".$_SESSION["User"]));
+$consulta = "SELECT Nombre FROM usuario WHERE Id=".$_SESSION["User"];
+$ejecutar = mysqli_query($GLOBALS["CONN"], $consulta);
+if(mysqli_num_rows($ejecutar)){
+	while($row=mysqli_fetch_assoc($ejecutar)){
+		$nombre = $row["Nombre"];
+	}
+}
+json(Array("Mensaje" => "Bienvenido ".$nombre));
+mysqli_close($GLOBALS["CONN"]);
 ?>
