466432dc83ce721a70236e50c1be2896a26c2f90
mbider <matibiderman@gmail.com>
2016-09-09 09:11:11 -0300

    Toursxgusto terminado bien

--- toursxgusto.php
+++ toursxgusto.php
@@ -1,25 +1,22 @@
 <?php
 require_once("conexion.php");
-
-$recibidos = $_GET["Idgusto"]; 
+$recibidos = $_GET["Idgusto"];
 
 $consulta = "SELECT T.Id, T.Nombre,T.Ubicacion,T.Foto, T.Descripcion, T.Likes, U.Nombre AS NombreUsuario, U.Id AS IdUsuario From tour T 
 INNER JOIN gustoxtour GT ON T.Id = GT.Idtour
 INNER JOIN gusto G ON G.Id = GT.Idgusto 
 INNER JOIN usuario U ON T.Idusuario = U.Id 
-Where GT.Idgusto IN (?) GROUP BY GT.Idtour";
+Where GT.Idgusto IN (".$recibidos.") GROUP BY GT.Idtour";
 $stmt = $GLOBALS["CONN"]->prepare($consulta);
-$stmt->bind_param(
-	"s",
-	$recibidos
-);
-$stmt->execute();
+$res=$stmt->execute();
 $resultado = $stmt->get_result();
+
+/*$cant = mysqli_num_rows($resultado);
+var_dump($cant);*/
+
 if($resultado){
 	$tours = LeerTours($resultado);
 	json($tours);	
 }
-
-
 mysqli_close($GLOBALS["CONN"]);
 ?>
