bc32989a810cef7f690239d2d19305553daf4228
mbider <matibiderman@gmail.com>
2016-10-31 09:19:15 -0300

    eliminar tour arreglado

--- EliminarTour.php
+++ EliminarTour.php
@@ -6,43 +6,37 @@ $Idtour = json_decode(file_get_contents('php://input'), true);
 $consulta = "DELETE  FROM tour WHERE Id = ?";
 
 $stmt = $GLOBALS["CONN"]->prepare($consulta);
-	var_dump($stmt);
-$id = $Idtour["Id"];
+$id = $Idtour["id"];
 $stmt->bind_param(
 	"s",
 	$id
 );
  
 $res = ejecutar($stmt);
-$puntos = $Idtour["Puntos"];
+$puntoid = $id;
 
 //CONSULTA PARA Eliminar PUNTOS
-foreach ($puntos as $punto) {
 	$consulta2 = "DELETE FROM punto WHERE Idtour = ?";
 	$res = ejecutar($stmt);
 	$stmt = $GLOBALS["CONN"]->prepare($consulta2);
 	$stmt->bind_param(
 		"s",
-		$id
+		$puntoid
 	);
 	
 	$res = ejecutar($stmt);
-
-}
-
 //CONSULTA PARA Elimianar GUSTOS
 
-$gustos = $Idtour["Gustos"];
+	$gustos = $id;
 
-foreach ($gustos as $gusto) {
 	$consulta3 = "DELETE FROM gustoxtour WHERE Idtour = ?";
 	$stmt = $GLOBALS["CONN"]->prepare($consulta3);
 	$stmt->bind_param(
 		"s",
-		$id
+		$gustos
 	);
 	$res = ejecutar($stmt);
-}
+
 
 
 if($res){
